7f08f7ae0d93b9799a980d788d0ac6e5c5b3595d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-27 21:01:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,7 @@ class Owner
   
   def sell_pets
     self.pets.each do |type, pet|
-      self[type].each {|animal| animal.mood = "nervous"}
+      self.pets[type].each {|animal| animal.mood = "nervous"}
     end
     self.pets.clear
   end
